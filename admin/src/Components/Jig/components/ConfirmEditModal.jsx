84ecf685393243d08501899624be1e63f6bfb688
GavFeng <gavfeng@uw.edu>
2026-02-08 16:55:35 -0800

    edit improve

--- admin/src/Components/Jig/components/ConfirmEditModal.jsx
+++ admin/src/Components/Jig/components/ConfirmEditModal.jsx
@@ -9,10 +9,10 @@ const renderJig = ({
   categories,
   weights
 }) => (
-  <div className="modal-jig-column">
+  <div className="modal-edit-column">
     <h3>{title}</h3>
 
-    <div className="modal-info">
+    <div className="modal-edit-meta">
       <p><strong>Name:</strong> {data.name}</p>
       <p><strong>Description:</strong> {data.description}</p>
       <p><strong>Price:</strong> ${data.price}</p>
@@ -46,7 +46,11 @@ const renderJig = ({
                 />
               ))}
             </div>
-            <p>{colors.find(co => co._id === c.color)?.name}</p>
+            <p>
+              {typeof c.color === "object"
+                ? c.color.name
+                : colors.find(co => co._id === c.color)?.name}
+            </p>
             <p>Stock: {c.stock}</p>
           </div>
         ))}
@@ -69,7 +73,7 @@ const ConfirmEditModal = ({
       <div className="modal-container modal-wide">
         <h2>Confirm Jig Changes</h2>
 
-        <div className="modal-compare-grid">
+        <div className="modal-edit-grid">
           {renderJig({
             title: "Current Jig",
             data: oldJig,
